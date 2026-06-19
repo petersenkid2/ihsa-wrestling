42030d24862309deae544a662ef5ea0f7ccd4047
petersenkid2 <Matt_petersen@Hotmail.com>
2026-06-19 10:36:02 -0500

    Update data-config.js

--- data-config.js
+++ data-config.js
@@ -1,3 +1,3 @@
 window.DATA_MODE = 'FULL'; // Switch to 'TEST' for smaller dataset
-window.DATA_FILE = window.DATA_MODE === 'TEST' ? 'data-test.js' : 'data-full.js';
+window.DATA_FILE = window.DATA_MODE === 'TEST' ? 'data-demo.js' : 'data-clean.js';
 window.DATA_VERSION = '1';
